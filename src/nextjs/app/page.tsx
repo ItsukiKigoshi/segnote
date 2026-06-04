bc726fcf20c5023ed92610fc7465444f55097120
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-06-04 22:33:39 +0900

    remove tailwindcss temporarily for mvp

--- src/nextjs/app/page.tsx
+++ src/nextjs/app/page.tsx
@@ -1,13 +1,17 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black ">
-            Segnote
-          </h1>
-        </div>
-      </main>
-    </div>
+    <main>
+      <article>
+        <h1>Segnote</h1>
+        <section>
+          <h2>Recordings</h2>
+          <ul>
+            <li>
+              <a href="/recs/test">Test Recording</a>
+            </li>
+          </ul>
+        </section>
+      </article>
+    </main>
   );
 }
